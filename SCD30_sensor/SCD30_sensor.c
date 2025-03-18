4f861a95fe0758d8b0e3b5877ca8e6deaa41a1c7
Dikshanya-Ram <dr655@cornell.edu>
2025-03-18 18:50:26 -0400

    initial setup for SCD30 sensor

---
+++ SCD30_sensor/SCD30_sensor.c
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ * 1. Dikshanya Ramaswamy
+ * 2. 
+ * 3. 
+ */
+
+#include "pico/stdlib.h"
+
+int main() {
+
+}
